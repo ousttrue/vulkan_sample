1e278175ac0788e05ab1b0508e4b7f64135aca07
ousttrue <ousttrue@gmail.com>
2022-12-22 21:16:27 +0900

    Impl

--- triangle/app.h
+++ triangle/app.h
@@ -1,117 +1,16 @@
 #pragma once
-#include <optional>
-#include <string>
 #include <vector>
 #include <vulkan/vulkan.h>
-#include <vulkan/vulkan_core.h>
 
 extern const bool enableValidationLayers;
 
-struct SwapChainSupportDetails {
-  VkSurfaceCapabilitiesKHR capabilities;
-  std::vector<VkSurfaceFormatKHR> formats;
-  std::vector<VkPresentModeKHR> presentModes;
-};
-
-struct QueueFamilyIndices {
-  std::optional<uint32_t> graphicsFamily;
-  std::optional<uint32_t> presentFamily;
-
-  bool isComplete() {
-    return graphicsFamily.has_value() && presentFamily.has_value();
-  }
-};
-
 class HelloTriangleApplication {
+  class Impl *impl_ = nullptr;
+
 public:
-  HelloTriangleApplication() {}
-  ~HelloTriangleApplication() {
-    vkDeviceWaitIdle(device);
-    cleanup();
-  }
+  HelloTriangleApplication();
+  ~HelloTriangleApplication();
+  VkInstance initVulkan(const std::vector<const char *> &extensions);
+  void createSwapChain(VkSurfaceKHR surface, int width, int height);
   void drawFrame();
-
-  VkInstance initVulkan(const std::vector<const char *> &extensions) {
-    createInstance(extensions);
-    setupDebugMessenger();
-    return instance;
-  }
-
-  void createSwapChain(VkSurfaceKHR surface, int width, int height) {
-    this->surface = surface;
-    pickPhysicalDevice();
-    createLogicalDevice();
-    createSwapChain(width, height);
-    createImageViews();
-    createRenderPass();
-    createGraphicsPipeline();
-    createFramebuffers();
-    createCommandPool();
-    createCommandBuffer();
-    createSyncObjects();
-  }
-
-private:
-  VkInstance instance;
-  VkDebugUtilsMessengerEXT debugMessenger;
-  VkSurfaceKHR surface;
-
-  VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
-  VkDevice device;
-
-  VkQueue graphicsQueue;
-  VkQueue presentQueue;
-
-  VkSwapchainKHR swapChain;
-  std::vector<VkImage> swapChainImages;
-  VkFormat swapChainImageFormat;
-  VkExtent2D swapChainExtent;
-  std::vector<VkImageView> swapChainImageViews;
-  std::vector<VkFramebuffer> swapChainFramebuffers;
-
-  VkRenderPass renderPass;
-  VkPipelineLayout pipelineLayout;
-  VkPipeline graphicsPipeline;
-
-  VkCommandPool commandPool;
-  VkCommandBuffer commandBuffer;
-
-  VkSemaphore imageAvailableSemaphore;
-  VkSemaphore renderFinishedSemaphore;
-  VkFence inFlightFence;
-
-  void cleanup();
-  void createInstance(const std::vector<const char *> &extensions);
-  void populateDebugMessengerCreateInfo(
-      VkDebugUtilsMessengerCreateInfoEXT &createInfo);
-  void setupDebugMessenger();
-  void pickPhysicalDevice();
-  void createLogicalDevice();
-  void createSwapChain(int width, int height);
-  void createImageViews();
-  void createRenderPass();
-  void createGraphicsPipeline();
-  void createFramebuffers();
-  void createCommandPool();
-  void createCommandBuffer();
-  void recordCommandBuffer(VkCommandBuffer commandBuffer, uint32_t imageIndex);
-  void createSyncObjects();
-  VkShaderModule createShaderModule(const std::vector<char> &code);
-  VkSurfaceFormatKHR chooseSwapSurfaceFormat(
-      const std::vector<VkSurfaceFormatKHR> &availableFormats);
-  VkPresentModeKHR chooseSwapPresentMode(
-      const std::vector<VkPresentModeKHR> &availablePresentModes);
-  VkExtent2D chooseSwapExtent(int width, int height,
-                              const VkSurfaceCapabilitiesKHR &capabilities);
-  SwapChainSupportDetails querySwapChainSupport(VkPhysicalDevice device);
-  bool isDeviceSuitable(VkPhysicalDevice device);
-  bool checkDeviceExtensionSupport(VkPhysicalDevice device);
-  QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device);
-  bool checkValidationLayerSupport();
-  static std::vector<char> readFile(const std::string &filename);
-  static VKAPI_ATTR VkBool32 VKAPI_CALL
-  debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
-                VkDebugUtilsMessageTypeFlagsEXT messageType,
-                const VkDebugUtilsMessengerCallbackDataEXT *pCallbackData,
-                void *pUserData);
 };
